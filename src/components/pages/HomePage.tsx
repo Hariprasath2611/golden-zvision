33fa0f0f7e4522040e8db18ebc9de6706795125c
Wix Remote Machine <support@wix.com>
2025-12-31 07:02:34 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -187,9 +187,8 @@ const HeroSection = () => {
           <div className="lg:col-span-8">
             <AnimatedElement delay={200}>
               <h1 className="font-heading text-6xl md:text-8xl lg:text-9xl font-bold leading-[0.9] tracking-tighter text-secondary mb-8">
-                Golden<br />
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-orange-600">Z</span><br />
-                Vision
+                GoldenZvision<br />
+                
               </h1>
             </AnimatedElement>
             
